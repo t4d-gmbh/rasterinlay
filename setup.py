2ec52069522315de9cc21bbcf9dee794e8e6a638
Jonas Liechti <jonas.i.liechti@protonmail.ch>
2020-12-27 00:59:34 +0100

    corrected classifiers

--- setup.py
+++ setup.py
@@ -110,7 +110,7 @@ setup(
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
           'Programming Language :: Python',
-          'Topic :: Scientific/Engineering :: GIS'
+          'Topic :: Scientific/Engineering :: GIS',
           'Topic :: Scientific/Engineering :: Image Processing'
     ],
     # include_package_data=True,  # includes whatever is in MANIFEST.in
